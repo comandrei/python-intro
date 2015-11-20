8baaf73880026ef1910e4dcfe829854e6f303460
Andrei Coman <andrei.coman@3pillarglobal.com>
2015-11-20 09:06:28 +0200

    Order by name stub

---
+++ order_by_name.py
@@ -0,0 +1,6 @@
+students =  # choose a data structure
+with open("students.csv", "r") as input_file:
+    for line in input_file:
+        # add the student name to the students
+for student in students:
+    print student
